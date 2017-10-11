73adfa5b92fae8ec1f15093580c658293f514413
Jeffrey Stocker <jeffreyRstocker@gmail.com>
2017-10-10 23:02:42 -0700

    Passed Invoke when provided with function reference

--- src/underbar.js
+++ src/underbar.js
@@ -409,6 +409,14 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map (collection, (element) => {
+      if (typeof functionOrKey === "string") {
+        var invoke = collection[functionOrKey];
+      } else {
+        var invoke = functionOrKey;
+      }
+      return invoke.apply(element, arguments)
+    });
   };
 
   // Sort the object's values by a criterion produced by an iterator.
